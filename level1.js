555b8ac3cdd167e1f850b72162466bd15b3bd98c
alifaltf <156301937+alifaltf@users.noreply.github.com>
2026-05-31 00:05:13 +0800

    Add files via upload

---
+++ level1.js
@@ -0,0 +1,34 @@
+function loadLevel1() {
+    let m2="", m3="", m4="", m5="";
+
+    if (currentTarget === "LR 5") {
+    
+        // --- MARKER 2: FIRST JUNCTION ---
+        m2 = `
+            <a-text value="TURN LEFT AND GO STRAIGHT UNTIL JUNCTION 2" position="0 1.5 0" align="center" color="#000000" rotation="-90 0 0" scale ="1 1 1"></a-text>
+            <a-entity geometry="primitive: cone" material="color: lime" rotation="-90 90 0" position="0 0.2 0" scale="0.5 0.7 0.5"></a-entity>`;
+
+        // --- MARKER 3: SECOND JUNCTION (Confirmation) ---
+        m3 = `
+            <a-text value="TURN LEFT" position="0 1.5 0" align="center" color="#000000" rotation="-90 0 0" scale ="1 1 1"></a-text>
+            <a-entity geometry="primitive: cone" material="color: lime" rotation="-90 90 0" position="0 0.2 0" scale="0.5 0.7 0.5"></a-entity>`;
+
+        // --- MARKER 4: FINAL JUNCTION ---
+        m4 = `
+            <a-text value="ENTER THE HALLWAY BEHIND YOUR BACK AND LR 5 IS ON YOUR RIGHT" position="0 1.5 0" align="center" color="#000000" rotation="-90 0 0"scale ="1 1 1"></a-text>
+            <a-entity geometry="primitive: cone" material="color: lime" rotation="-90 180 0" position="0 0.2 0" scale="0.5 0.7 0.5"></a-entity>`;
+
+        // --- MARKER 5: ARRIVAL (LR 5) ---
+        m5 = `
+            <a-text value="DESTINATION REACHED" position="0 2.5 0" align="center" color="#00FF00" rotation="-90 0 0" scale ="1 1 1"></a-text>
+            <a-text value="LR 5" position="0 1.8 0" align="center" color="white" rotation="-90 0 0""></a-text>
+            <a-entity geometry="primitive: sphere" material="color: gold" animation="property: scale; to: 0.5 0.5 0.5; dir: alternate; loop: true"></a-entity>`;
+    }
+
+    return `
+        <a-marker type="barcode" value="2">${m2}</a-marker>
+        <a-marker type="barcode" value="3">${m3}</a-marker>
+        <a-marker type="barcode" value="4">${m4}</a-marker>
+        <a-marker type="barcode" value="5">${m5}</a-marker>
+    `;
+}
---
+++ level2.js
@@ -0,0 +1,25 @@
+function loadLevel2() {
+    let m7="", m8="", m9="";
+    if (currentTarget === "MPH") {
+
+        m7 = `
+            <a-text value="TURN RIGHT THE TURN LEFT TO THE HALLWAY" position="0 1.5 0" align="center" color="#000000" look-at="[camera]" scale ="1 1 1"></a-text>
+            <a-entity geometry="primitive: cone" material="color: lime" rotation="-90 -90 0" position="0 0.2 0" scale="0.5 0.7 0.5"></a-entity>`;
+
+       
+        m8 = `
+            <a-text value="TAKE THE STAIRS TO LEVEL 2" position="0 1.5 0" align="center" color="#000000" look-at="[camera]" scale ="1 1 1"></a-text>
+            <a-entity geometry="primitive: cone" material="color: lime" rotation="-90 0 0" position="0 0.2 0" scale="0.5 0.7 0.5"></a-entity>`;
+
+        m9 = `
+            <a-text value="DESTINATION REACHED" position="0 2.5 0" align="center" color="#00FF00" look-at="[camera]" scale ="1 1 1"></a-text>
+            <a-text value="MPH" position="0 1.8 0" align="center" color="white" look-at="[camera]"></a-text>
+            <a-entity geometry="primitive: sphere" material="color: gold" animation="property: scale; to: 0.5 0.5 0.5; dir: alternate; loop: true"></a-entity>`;
+    }
+
+    return `
+        <a-marker type="barcode" value="7">${m7}</a-marker>
+        <a-marker type="barcode" value="8">${m8}</a-marker>
+        <a-marker type="barcode" value="9">${m9}</a-marker>
+    `;
+}
